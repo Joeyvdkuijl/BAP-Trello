b7ad898d50bbf5905f1f3cf7c131ee77c523e574
Joey van der Kuijl <joeyvdkuijl@hotmail.com>
2020-12-17 02:44:36 +0100

    re routing homepage

--- resources/views/blog/overzicht.blade.php
+++ resources/views/blog/overzicht.blade.php
@@ -9,4 +9,15 @@
 <a href="{{ route('info.aboutme') }}"><button type="submit">Info</button></a>
 <a href="{{ route('project.info') }}"><button type="submit">Project</button></a>
 <a href="{{ route('from.contact') }}"><button type="submit">Contact</button></a>
+
+<a  href="{{ route('logout') }}"
+onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+ <button>Logout</button>
+</a>
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+ @csrf
+</form>
+
 @endsection
